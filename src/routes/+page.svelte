12ddb209451fcd4df4b14d49c579a513193d0e99
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2025-02-24 14:12:56 +1300

    Divs and classes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,22 +6,31 @@
 <Header />
 
 <main>
-  <h2>Lorem Ipsum with Flavour</h2>
+  <div class="text">
+    <h2>Lorem Ipsum with Flavour</h2>
 
-  <p>What's Ipsum Bro is a lorem ipsum generator with a twist. Instead of the same old boring lorem ipsum, you can choose from a variety of flavours to spice up your placeholder text.</p>
-
-  <a href="pirate">
-    <img class="box" src="pirate.png" alt="Pirate" />
-  </a>
-  <a href="cheese">
-    <img class="box" src="cheese.png" alt="Cheese" />
-  </a>
-  <a href="cupcake">
-    <img class="box" src="cupcake.png" alt="Cupcake" />
-  </a>
-  <a href="hipster">
-    <img class="box" src="hipster.png" alt="Hipster" />
-  </a>
+    <p>What's Ipsum Bro is a lorem ipsum generator with a twist. Instead of the same old boring lorem ipsum, you can choose from a variety of flavours to spice up your placeholder text.</p>
+  </div>
+  <div class="boxes">
+    <a href="pirate">
+      <img class="box" src="pirate.png" alt="Pirate" />
+    </a>
+  </div>
+  <div class="boxes">
+    <a href="cheese">
+      <img class="box" src="cheese.png" alt="Cheese" />
+    </a>
+  </div>
+  <div class="boxes">
+    <a href="cupcake">
+      <img class="box" src="cupcake.png" alt="Cupcake" />
+    </a>
+  </div>
+  <div class="boxes">
+    <a href="hipster">
+      <img class="box" src="hipster.png" alt="Hipster" />
+    </a>
+  </div>
 </main>
 
 <Footer />
@@ -34,5 +43,13 @@
     padding: 25px;
     background: ivory;
     box-shadow: 15px 15px lightblue;
+  }
+
+  .boxes {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 25px;
+    justify-content: space-between;
+    width: 400px;
   }
 </style>
