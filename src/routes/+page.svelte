50a04a9b3ae2d60459af3913e68441883d9115b1
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2025-02-25 14:46:02 +1300

    Column layout on landing page.
    Home page now has a side-by-side layout using Flexbox, which changes to vertical on mobile.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,30 +6,31 @@
 <Header />
 
 <main>
-  <div class="text">
-    <h2>Lorem Ipsum with Flavour</h2>
-
-    <p>What's Ipsum Bro is a lorem ipsum generator with a twist. Instead of the same old boring lorem ipsum, you can choose from a variety of flavours to spice up your placeholder text.</p>
-  </div>
-  <div class="boxes">
-    <a href="pirate">
-      <img class="box" src="pirate.png" alt="Pirate" />
-    </a>
-  </div>
-  <div class="boxes">
-    <a href="cheese">
-      <img class="box" src="cheese.png" alt="Cheese" />
-    </a>
-  </div>
-  <div class="boxes">
-    <a href="cupcake">
-      <img class="box" src="cupcake.png" alt="Cupcake" />
-    </a>
-  </div>
-  <div class="boxes">
-    <a href="hipster">
-      <img class="box" src="hipster.png" alt="Hipster" />
-    </a>
+  <div class="columns">
+    <div class="text">
+      <h2>Lorem Ipsum with Flavour</h2>
+      <p>What's Ipsum Bro is a lorem ipsum generator with a twist. Instead of the same old boring lorem ipsum, you can choose from a variety of flavours to spice up your placeholder text.</p>
+    </div>
+    <div class="boxes">
+      <a href="pirate">
+        <img class="box" src="pirate.png" alt="Pirate" />
+      </a>
+    </div>
+    <div class="boxes">
+      <a href="cheese">
+        <img class="box" src="cheese.png" alt="Cheese" />
+      </a>
+    </div>
+    <div class="boxes">
+      <a href="cupcake">
+        <img class="box" src="cupcake.png" alt="Cupcake" />
+      </a>
+    </div>
+    <div class="boxes">
+      <a href="hipster">
+        <img class="box" src="hipster.png" alt="Hipster" />
+      </a>
+    </div>
   </div>
 </main>
 
@@ -56,5 +57,28 @@
   .text {
     font-family: "Patrick Hand", cursive;
     width: 30%;
+  }
+
+  h2 {
+    text-align: center;
+  }
+
+  .columns {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    background: lavender;
+    padding: 5rem 10rem;
+  }
+
+  @media screen and (max-width: 600px) {
+    .columns {
+      flex-direction: column;
+      padding: 2rem 3rem;
+    }
+
+    .text {
+      width: 100%;
+    }
   }
 </style>
