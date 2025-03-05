66422d15d2b9fc6e975291c08c16cab7607fed85
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2025-03-06 09:44:30 +1300

    cheeeeeseeeeee

--- src/routes/cheese/+page.svelte
+++ src/routes/cheese/+page.svelte
@@ -7,11 +7,15 @@
 <svelte:window bind:scrollY={y} bind:innerHeight={height} />
 
 <main>
-  <h2>Pee-YOO Cheese Ipsum</h2>
+  <img class="is-rounded" src="c-for-cheese.png" alt="Cheese heading with round cheese for the C" />
 
-  <p>Cheesy grin parmesan taleggio. Brie boursin pecorino cheesy feet taleggio hard cheese roquefort when the cheese comes out everybody's happy. St. agur blue cheese port-salut bocconcini cheesecake rubber cheese mozzarella stilton cheese strings. Bocconcini.</p>
+  <h2>heese Ipsum</h2>
 
-  <p>Fromage the big cheese camembert de normandie. Cut the cheese gouda brie the big cheese when the cheese comes out everybody's happy squirty cheese cut the cheese gouda. Boursin cottage cheese cheeseburger camembert de normandie cheese slices goat port-salut danish fontina. Emmental caerphilly cream cheese mozzarella camembert de normandie cheesy feet.</p>
+  <div class="text-background">
+    <p>Cheesy grin parmesan taleggio. Brie boursin pecorino cheesy feet taleggio hard cheese roquefort when the cheese comes out everybody's happy. St. agur blue cheese port-salut bocconcini cheesecake rubber cheese mozzarella stilton cheese strings. Bocconcini.</p>
+
+    <p>Fromage the big cheese camembert de normandie. Cut the cheese gouda brie the big cheese when the cheese comes out everybody's happy squirty cheese cut the cheese gouda. Boursin cottage cheese cheeseburger camembert de normandie cheese slices goat port-salut danish fontina. Emmental caerphilly cream cheese mozzarella camembert de normandie cheesy feet.</p>
+  </div>
 
   <aside style="transform: rotate({scrollPercentage * 1000}deg);"></aside>
 </main>
@@ -26,8 +30,31 @@
     background-color: #dc4d3a;
   }
 
+  img {
+    height: 150px;
+    width: 150px;
+    border-radius: 50%;
+  }
+
+  p {
+    font-family: "Irish Grover", system-ui;
+    color: #f7d571;
+    margin-left: 20%;
+    margin-right: 30%;
+    transform: rotate(-10deg);
+  }
+
+  .text-background {
+    background-image: url("Gouda-block-slice.png");
+    background-repeat: no-repeat;
+    background-position: center;
+    padding: 200px;
+  }
   h2 {
     color: #f2b329;
+    font-family: "Irish Grover", system-ui;
+    font-size: 1000%;
+    text-align: center;
   }
 
   aside {
