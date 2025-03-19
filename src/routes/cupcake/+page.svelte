73fff6f4bfde7e1bcad0a43571c6248907cc9100
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2025-03-20 09:43:59 +1300

    Explosion sorted
    It looks good.

--- src/routes/cupcake/+page.svelte
+++ src/routes/cupcake/+page.svelte
@@ -14,8 +14,13 @@
       <p>Halvah liquorice apple pie jelly beans chupa chups candy icing tiramisu. Tart jelly jujubes gingerbread croissant biscuit. Chupa chups tart cheesecake lemon drops dragée chocolate bar dessert. Ice cream ice cream sugar plum jujubes dragée topping tiramisu croissant chocolate.</p>
       <p>Jujubes tiramisu gingerbread cake donut chocolate bar tart marzipan pastry. Gingerbread jelly tootsie roll muffin bear claw muffin. Jelly-o fruitcake chocolate bar chupa chups chocolate sesame snaps candy. Sweet roll carrot cake cake jelly candy jujubes gummi bears jelly-o. Oat cake wafer chocolate bar sweet roll macaroon soufflé powder sugar plum tart.</p>
     </div>
-    <img src="Cupcake-notlit-sparkler.png" alt="Cupcake with sparkler on top" style="left:15vh;" />
-    <img src="sparkle.png" alt="Sparkler light" style="top: {45 + scrollPercentage * 20}vh; left: {scrollPercentage * 30}vh;" />
+    {#if scrollPercentage >= 0.37}
+      <div class="explosion" style="top: 10vh;"></div>
+    {/if}
+    {#if scrollPercentage < 0.37}
+      <img src="Cupcake-notlit-sparkler.png" alt="Cupcake with sparkler on top" style="left:15vh;" />
+      <img src="sparkle.png" alt="Sparkler light" style="top: {45 + scrollPercentage * 40}vh; left: {scrollPercentage * 60}vh;" />
+    {/if}
   </div>
 </main>
 
@@ -29,8 +34,8 @@
 
   p {
     color: #ffdff4;
-    margin-right: 50px;
-    margin-left: 600px;
+    margin-right: 200px;
+    margin-left: 700px;
     font-size: 325%;
     font-family: "Lily Script One", system-ui;
   }
@@ -41,9 +46,18 @@
     height: 200px;
   }
 
+  .explosion {
+    position: fixed;
+    margin-left: 40px;
+    height: 800px;
+    width: 800px;
+    background-image: url("giphy-explosion.gif");
+    background-repeat: no-repeat;
+  }
+
   main {
     background-color: #8b3683;
-    height: 10000px;
+    height: 5000px;
   }
 
   .text {
@@ -53,4 +67,7 @@
   .cupcake_layout_div {
     display: flex;
   }
+
+  /*Need to make div not appear until it hits a certain scroll percentage and at that point, hide the cupcake and sparkler.
+  div code previously used*/
 </style>
