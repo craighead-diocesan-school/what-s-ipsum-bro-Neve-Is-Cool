5a76ee34242657d2dea29ab7a5e1f33411a2101e
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2025-03-24 14:21:06 +1300

    progress.

--- src/routes/hipster/+page.svelte
+++ src/routes/hipster/+page.svelte
@@ -1,7 +1,48 @@
 <main>
-  <h2>You Probably Haven't Heard of Hipster Ipsum</h2>
+  <div class="top">
+    <h2>You Probably Haven't Heard of Hipster Ipsum</h2>
 
-  <p>Fingerstache tofu pickled vibecession kitsch lyft blackbird spyplane vegan craft beer keytar banh mi thundercats. Hashtag heirloom vape scenester paleo hexagon. Blackbird spyplane pok pok DIY heirloom scenester food truck kale chips pabst, bitters raw denim artisan waistcoat seitan freegan.</p>
-  
-  <p>Put a bird on it vibecession fashion axe YOLO asymmetrical. VHS photo booth listicle fam truffaut subway tile yr cred chicharrones semiotics biodiesel green juice microdosing whatever. Truffaut praxis PBR and B tattooed street art. Prism jean shorts pickled vegan. Vegan praxis cronut kitsch pop-up. Letterpress street art mlkshk marxism.</p>
+    <div class="text">
+      <div class="left-text">
+        <p>Fingerstache tofu pickled vibecession kitsch lyft blackbird spyplane vegan craft beer keytar banh mi thundercats. Hashtag heirloom vape scenester paleo hexagon. Blackbird spyplane pok pok DIY heirloom scenester food truck kale chips pabst, bitters raw denim artisan waistcoat seitan freegan.</p>
+      </div>
+      <p>Put a bird on it vibecession fashion axe YOLO asymmetrical. VHS photo booth listicle fam truffaut subway tile yr cred chicharrones semiotics biodiesel green juice microdosing whatever. Truffaut praxis PBR and B tattooed street art. Prism jean shorts pickled vegan. Vegan praxis cronut kitsch pop-up. Letterpress street art mlkshk marxism.</p>
+    </div>
+  </div>
+  <div class="bottom"></div>
 </main>
+
+<style>
+  h2 {
+    font-family: "Monoton", sans-serif;
+    transform: rotate(-10deg);
+    padding: 5%;
+    font-size: 450%;
+    text-align: center;
+  }
+
+  p {
+    font-family: "Marko One", serif;
+    margin-left: 10%;
+    margin-right: 5%;
+    font-size: 120%;
+  }
+  .top {
+    background-color: #f8e4c2;
+    color: #7f353c;
+    height: 700px;
+  }
+
+  .text {
+    display: flex;
+  }
+
+  .left-text {
+    margin-top: 40px;
+  }
+
+  .bottom {
+    background-color: #7f353c;
+    height: 200px;
+  }
+</style>
